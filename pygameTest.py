578de88d5b51fa7ea19d4c931c9786506e97e27f
MrFichter <jonathan.fichter@live.com>
2012-08-19 18:29:31 -0400

    walkingMan1 now appears.
    Left off: video #2, 5 minutes (exactly) into it.

--- pygameTest.py
+++ pygameTest.py
@@ -4,12 +4,36 @@
 #raw_input ('Press Enter key to exit.')
 #exit ()
 
-import sys, pygame
+import sys, pygame #Remember: sudo apt-get install python-pygame
 pygame.init()
+
 
 #Create a window.
 size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
-#exit
+
+#Load an image file.
+walkingMan1 = pygame.image.load('scratchManWalking1.png') #I tried opening this file when it was located in another directory, but I had a hard time with this.
+
+
+#Make the image appear on the screen.
+screen.blit(walkingMan1, (200, 200)) #Parameters indicate coordinates of top left corner of the image.
+
+
+pygame.display.flip() #Redraws the screen.
+while 1: #Forever loop.
+
+	
+	pygame.display.flip() #Redraws the screen.
+
+	
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit() # Closing the window ends the script.
+
+
+
+#backup plan for exiting
 raw_input ('Press Enter key to exit.')
