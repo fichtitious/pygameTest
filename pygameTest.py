f76480af1e1cadf0e7f50058d02599a76f52467c
MrFichter <jonathan.fichter@live.com>
2012-08-19 11:35:21 -0400

    Created pygameTest
    Set up some code in the first few lines so that I can
    test small pieces of code and then have it exit().
    Learning note: I'm using control + z to background
    the program so that I can run it from the command line.
    Then I type fg or fg 1 to move it to the foreground again.

---
+++ pygameTest.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+#Use this area for testing small pieces of code
+raw_input ('Press Enter key to exit.')
+exit ()
+
+
+print 'hello world'
+
+#exit
+raw_input ('Press Enter key to exit.')
