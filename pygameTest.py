4819ef8bf7e2e47192c741de38c9d981b462d75b
MrFichter <jonathan.fichter@live.com>
2012-08-26 14:27:41 -0400

    Animation now works.
    The walking man now moves from one point to another.
    Next steps: 1) Move as much as you can into
    functions rather than using the global namespace.
    2) Can you get the character to move and to do a
    costume change? 3) Experiment with collisions.

--- pygameTest.py
+++ pygameTest.py
@@ -13,26 +13,44 @@ size = width, height = 600, 400
 screen = pygame.display.set_mode(size)
 
 
+print '''
+Welcome to my game. 
+To exit, please close the window
+in which the animation appears.
+
+'''
+
 #Load an image file.
 walkingMan1 = pygame.image.load('scratchManWalking1.png') #I tried opening this file when it was located in another directory, but I had a hard time with this.
 
 
-#Make the image appear on the screen.
-screen.blit(walkingMan1, (200, 200)) #Parameters indicate coordinates of top left corner of the image.
+x = 0 #horizontal coordinate for where top left of image will apppear
+y = 0 #vertical coordinate for where top left of image will apppear
+
+black = 0 , 0 , 0 #a color we'll use when we black out the screen below
 
 
 pygame.display.flip() #Redraws the screen.
 while 1: #Forever loop.
 
-	
-	pygame.display.flip() #Redraws the screen.
-
-	
-
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit() # Closing the window ends the script.
 
+
+	screen.fill(black) #blacks out the screen using the color we specified above
+	
+
+	#Make the image appear on the screen.
+	screen.blit(walkingMan1, (x, y)) #Parameters indicate coordinates of top left corner of the image.
+
+	pygame.display.flip() #Redraws the screen.
+	
+	#Change horizontal and vertical coordinates so that the image moves.
+	x += 1
+	y += 1
+	
+	
 
 
 #backup plan for exiting
